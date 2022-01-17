e76cd96d74810b5666557f07b2ba9edce32895c3
leejh16 <cheny5dyh@gmail.com>
2022-01-18 07:32:11 +0900

    match/exchange contract-20220118

--- src/config/addresses.js
+++ src/config/addresses.js
@@ -1,7 +1,10 @@
 
 const NETTYPE='KLAYTN-TESTNET'  // baobab
 const ADDRESSES={
-	erc1155 : '0xb7aa9cd318e97f42a477dc1d9185fdec5503e9b5'
+
+		match : '0x052773017abe46bffaeafb82cd464099d2a7883b'
+	, exchange : '0x052773017abe46bffaeafb82cd464099d2a7883b'
+	, erc1155 : '0xb7aa9cd318e97f42a477dc1d9185fdec5503e9b5'
 	, admin : '0xde29fcd765b0963237e66faf0f043e6634c38f42'
 	, vault : '0x350794afb661ca9d7ac6d40fc939100d26262dee'
 	, blackwhitelist : '0xb4ac8172c6e09fb0304442f02d6fe8c3d1828186'
