85a900a58393b3765ac025e5d892683a715161ea
bakujinDev <87050600+bakujinDev@users.noreply.github.com>
2022-02-03 15:10:02 +0900

    back to patch main category 22.01.24

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Main from "./Main";
 import Header from "./Header";
 import ConnectWallet from "./router/ConnectWallet";
 import EmailRequired from "./router/EmailRequired";
-import RecentEmail from "./router/RecentEmail";
 import JoinMemberShip from "./router/JoinMemberShip";
 import Signup from "./router/Signup";
 import EmailFailed from "./router/EmailFailed";
@@ -24,39 +23,18 @@ import CreateItem from "./router/CreateItem";
 import SaleFixed from "./router/SaleFixed";
 import AuctionBid from "./router/AuctionBid";
 import SaleBundle from "./router/SaleBundle";
-// import MarketPlaceItem14 from "./router/MarketPlace_item14";
-// import ItemInfo01 from "./router/ItemInfo01";
-// import ItemInfo02 from "./router/ItemInfo02";
-// import EditItem from "./router/EditItem";
-// import ItemInfo04 from "./router/ItemInfo04";
-// import Nftsell04 from "./router/Nftsell04";
-
-// import Nftsell05 from "./router/VerifyAccountPopup";
-// import Nftsell05Off from "./router/Nftsell05Off";
-// import Buynft01 from "./router/Buynft01";
-// import Buynft02 from "./router/Buynft02";
-// import Buynft03 from "./router/Buynft03";
-// import Buynft04 from "./router/Buynft04";
 import MyProf from "./router/MyProf";
-// import Profile02 from "./router/SaleFixed";
-// import Profile03 from "./router/Profile03";
 import HandOver from "./router/HandOver";
-// import Profile05 from "./router/Profile05";
 import MoveCollection from "./router/MoveCollection";
 import TransactionHistory from "./router/TransactionHistory";
-// import Profile07Off from "./router/Profile07Off";
 import Offers from "./router/Offers";
 import Liked from "./router/Liked";
-// import Profile13 from "./router/Profile13";
 import HiddenItem from "./router/HiddenItem";
 import Referals from "./router/Referals";
 import Mywallet from "./router/Mywallet";
 import VerifyEmail from "./router/VerifyEmail";
-// import Profile17 from "./router/Profile17";
-// import LogOut from "./router/LogOut";
 import GeneralSettings from "./router/GeneralSettings";
 import NotificationSettings from "./router/NotificationSettings";
-// import MProfileMenu from "./router/MProfileMenu";
 import ExploreDeal from "./router/ExploreDeal";
 import Ranking from "./router/Ranking";
 import MheaderPopup from "./components/MheaderPopup";
@@ -69,31 +47,47 @@ import { useEffect } from "react";
 import axios from "axios";
 import SetErrorBar from "./util/SetErrorBar";
 import { API } from "./config/api";
-import { MSG } from "./config/messages";
+import { messages } from "./config/messages";
 import { SET_ADDRESS } from "./reducers/walletSlice";
 import { GET_USER_DATA } from "./reducers/userSlice";
 import GlobalStyle from "./components/globalStyle";
-
-function App({ store, setHref, setConnect }) {
+import { setmyinfo, setaddress } from "./util/store";
+import { LOGGER, PARSER, STRINGER } from "./util/common";
+function App({ store, setHref, setConnect, Setmyinfo, Setaddress }) {
   const dispatch = useDispatch();
   const { userData } = useSelector((state) => state.user);
   const { mHeaderPopup } = useSelector((state) => state.store);
-
   const on_wallet_disconnect = (_) => {
     let token_sec = localStorage.getItem("token");
+    if (token_sec) {
+    } else {
+      LOGGER("h9T8jyxy0L@no token");
+      return;
+    }
     axios.defaults.headers.get.token = token_sec;
     axios.defaults.headers.post.token = token_sec;
-    axios.post(API.API_LOGOUT).then((resp) => {
-      let { status } = resp.data;
-      if (status === "OK") {
-        axios.defaults.headers.common["token"] = "";
-        localStorage.removeItem("token");
-      }
+    return new Promise((resolve, reject) => {
+      axios.post(API.API_LOGOUT).then((resp) => {
+        LOGGER("3LyOB7GcWr@logout", resp.data);
+        let { status } = resp.data;
+        if (status == "OK") {
+          //          SetLogut();
+          localStorage.removeItem("token");
+          resolve(true);
+        } else {
+          resolve(null);
+        }
+      });
     });
   };
-
   const get_user_data = async () => {
     const token = localStorage.getItem("token");
+    let str_myinfo;
+    if (store.myinfo) {
+    } else if ((str_myinfo = localStorage.getItem("myinfo"))) {
+      Setmyinfo(PARSER(str_myinfo));
+    }
+    return;
     if (token) {
       try {
         axios.defaults.headers.common["token"] = token;
@@ -109,41 +103,58 @@ function App({ store, setHref, setConnect }) {
     }
   };
 
-  useEffect(() => {
-    window.klaytn.on("accountsChanged", (accounts) => {
-      console.log(accounts);
-      if (accounts[0]) {
-        dispatch({ type: SET_ADDRESS.type, payload: accounts[0] });
-        let address = accounts[0];
-        axios
-          .post(API.API_USERS_LOGIN, { address: address, cryptotype: "ETH" })
-          .then((resp) => {
-            let { status, respdata } = resp.data;
-            if (status === "OK") {
-              localStorage.setItem("token", respdata);
-              axios.defaults.headers.common["token"] = resp.data.respdata;
-            } else if (status === "ERR") {
-              localStorage.removeItem("token");
-              axios.defaults.headers.common["token"] = "";
-            }
-          });
+  useEffect(
+    async (_) => {
+      let { klaytn } = window;
+      if (klaytn) {
       } else {
-        SetErrorBar(MSG.MSG_WALLET_DISCONNECTED);
-        on_wallet_disconnect();
+        return;
       }
-    });
-  }, [window.klaytn]);
+      klaytn.on("accountsChanged", async (accounts) => {
+        console.log(accounts);
+        let address = accounts[0];
+        let address_local = localStorage.getItem("address");
+        if (address == address_local) {
+          return;
+        } else {
+        }
+        await on_wallet_disconnect();
+        if (address) {
+          //        disp atch({ type: SET_ADDRESS.type, payload: accounts[0] });				//				let address = accounts[0]
+          Setaddress(address);
+          axios
+            .post(API.API_USERS_LOGIN, { address: address, cryptotype: "ETH" })
+            .then((resp) => {
+              let { status, respdata } = resp.data;
+              if (status === "OK") {
+                localStorage.setItem("token", respdata);
+                axios.defaults.headers.common["token"] = resp.data.respdata;
+              } else if (status === "ERR") {
+                localStorage.removeItem("token");
+                axios.defaults.headers.common["token"] = "";
+              }
+            });
+        } else {
+          SetErrorBar(messages.MSG_WALLET_DISCONNECTED);
+          on_wallet_disconnect();
+        }
+      });
+    },
+    [window.klaytn]
+  );
 
   useEffect(() => {
-    if (window.klaytn.selectedAddress) {
-      dispatch({
-        type: SET_ADDRESS.type,
-        payload: window.klaytn.selectedAddress,
-      });
-      if (userData === null) {
-        get_user_data();
+    LOGGER("poMFHstZg8", window.klaytn?.selectedAddress);
+    setTimeout((_) => {
+      if (window.klaytn?.selectedAddress) {
+        Setaddress(
+          window.klaytn?.selectedAddress
+        ); /**       dispa tch({        type: SET_ADDRESS.type,        payload: window.klaytn.selectedAddress,			});			*/
+        if (userData === null) {
+          get_user_data();
+        }
       }
-    }
+    }, 10 * 1000);
   }, []);
 
   return (
@@ -174,9 +185,7 @@ function App({ store, setHref, setConnect }) {
 
         <Routes>
           <Route path="/index" element={<List />} />
-
           <Route path="/" element={<Main />} />
-
           <Route path="/connectwallet" element={<ConnectWallet />} />
           <Route path="/emailrequired" element={<EmailRequired />} />
           <Route path="/sentemail" element={<SentEmail />} />
@@ -227,8 +236,6 @@ function App({ store, setHref, setConnect }) {
           {/*
           <Route path="/edititem" element={<EditItem />} />
           <Route path="/mprofilemenu" element={<MProfileMenu />} />
-
-
           */}
         </Routes>
 
@@ -243,19 +250,16 @@ const AppBox = styled.div`
   width: 100%;
   background: #fff;
   position: relative;
-
   a,
   img {
     cursor: pointer;
   }
-
   .selectList {
     li {
       display: flex;
       align-items: center;
       gap: 20px;
       cursor: pointer;
-
       .chkBtn {
         display: flex;
         justify-content: center;
@@ -264,7 +268,6 @@ const AppBox = styled.div`
         height: 18px;
         border: solid 2px #d9d9d9;
         border-radius: 50%;
-
         span {
           width: 8px;
           height: 8px;
@@ -279,9 +282,31 @@ const AppBox = styled.div`
 function mapStateToProps(state) {
   return { store: state };
 }
-
 function mapDispatchToProps(dispatch) {
-  return {};
+  return {
+    Setmyinfo: (payload) => dispatch(setmyinfo(payload)),
+    Setaddress: (payload) => dispatch(setaddress(payload)),
+  };
 }
-
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// import MarketPlaceItem14 from "./router/MarketPlace_item14";
+// import ItemInfo01 from "./router/ItemInfo01";
+// import ItemInfo02 from "./router/ItemInfo02";
+// import EditItem from "./router/EditItem";
+// import ItemInfo04 from "./router/ItemInfo04";
+// import Nftsell04 from "./router/Nftsell04";
+// import Nftsell05 from "./router/VerifyAccountPopup";
+// import Nftsell05Off from "./router/Nftsell05Off";
+// import Buynft01 from "./router/Buynft01";
+// import Buynft02 from "./router/Buynft02";
+// import Buynft03 from "./router/Buynft03";
+// import Buynft04 from "./router/Buynft04";
+// import Profile02 from "./router/SaleFixed";
+// import Profile03 from "./router/Profile03";
+// import Profile05 from "./router/Profile05";
+// import Profile07Off from "./router/Profile07Off";
+// import Profile13 from "./router/Profile13";
+// import Profile17 from "./router/Profile17";
+// import LogOut from "./router/LogOut";
+// import MProfileMenu from "./router/MProfileMenu";
--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,5 @@
-const apiServer = "http://3.34.76.233:32287";
+const apiServer = "http://itemverse1.net:32287";
+
 export const API = {
   API_USER_JOIN: `${apiServer}/users/join`,
   API_COMPLETE_EMAIL_VERIFY: `${apiServer}/users/bind_address_email`,
@@ -8,9 +9,16 @@ export const API = {
   API_LOGOUT: `${apiServer}/users/logout`,
   API_ITEM_UPLOAD_BASE64: `${apiServer}/mint/store/base64`,
   API_ITEM_UPLOAD_OVER: `${apiServer}/mint/store/file`,
-  API_GET_ITEM_DATA: `${apiServer}/items/item`,
+  API_GET_ITEM_DATA: `${apiServer}/items/item`, // /:itemid
   API_ITEM_SAVE_META: `${apiServer}/mint/metadata`,
   API_MINT_TX_REPORT: `${apiServer}/mint/report/mint`,
   API_LAZY_MINT: `${apiServer}/mint/mint`,
   API_GET_ITEM_CATEGORIES: `${apiServer}/queries/categories`,
+  API_GET_EXPLORE: `${apiServer}/queries/rows/fieldvalues/transactions/0/10/id/DESC`,
+  API_GET_ITEM_LIST: `${apiServer}/merchandises`,
+  API_GET_LIKE_LIST: `${apiServer}/queries/rows/logfavorites/objectid`, // /:itemid /:index /:limit /id /:sort
+  API_GET_OWNER_LIST: `${apiServer}/queries/rows/itembalances/itemid`, // /:itemid /:index /:limit /id /:sort
+
+  API_ORDER_MAKER_SELLER: `${apiServer}/orders/maker/seller`,
+  API_PLATFORM_SETTINGS: `${apiServer}/queries/rows/settings/active/1/0/100/id/DESC`,
 };
